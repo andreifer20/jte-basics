1fb5aade388b6074ce5baa4cb19dfb512d221a31
andreifer20 <43587902+andreifer20@users.noreply.github.com>
2023-05-05 08:26:09 +0400

    nmmnmn

--- libraries/sonarqube/steps/static_code_analysis.groovy
+++ libraries/sonarqube/steps/static_code_analysis.groovy
@@ -1,5 +1,5 @@
 void call(){
-    stage("SonarQube: Staatic Code Analysis"){
+    stage("SonarQube: Staatic Code Analysis-FER"){
         println "static code analysis from the sonarqube library"
     }
 }
