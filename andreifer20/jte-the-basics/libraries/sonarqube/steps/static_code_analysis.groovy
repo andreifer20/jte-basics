d1016e48e3ec3d32c2d8e1d8cf9761fed6b75227
andreifer20 <43587902+andreifer20@users.noreply.github.com>
2023-05-05 08:16:18 +0400

    asdad

--- andreifer20/jte-the-basics/libraries/sonarqube/steps/static_code_analysis.groovy
+++ andreifer20/jte-the-basics/libraries/sonarqube/steps/static_code_analysis.groovy
@@ -1,5 +1,5 @@
 void call(){
-    stage("SonarQube: Static Code Analysis"){
+    stage("SonarQube: Staatic Code Analysis"){
         println "static code analysis from the sonarqube library"
     }
 }
